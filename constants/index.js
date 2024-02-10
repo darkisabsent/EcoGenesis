27f778f6d3bd23d6e0600bb1a7ec68f522e1fadc
selim thabet <thabetselim06@gmail.com>
2024-02-10 19:47:12 +0100

    working

---
+++ constants/index.js
@@ -0,0 +1,80 @@
+export const exploreWorlds = [
+ 
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Recycle Water',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Our System',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'The treated water has multiple uses',
+  },
+  
+];
+
+export const startingFeatures = [
+  'Accessible and affordable',
+  'Flexible',
+  'Scalable',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Vision',
+    subtitle:
+        'EcoGenesis strives to secure access to quality water for Tunisians, while also transforming waste into nutrient-rich soil and envisioning a future with reduced dependence on plastic bottles.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Commitment',
+    subtitle:
+        "Our mission involves treating wastewater for reuse, relieving pressure on water providers, raising awareness and fostering a global commitment to responsible water management and environmental preservation.  ",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/water-treatment-01.png',
+    title: 'Revolutionizing Water Treatment',
+    subtitle:
+        "Dive into the future of water management with our revolutionary treatment systems. From advanced filtration to sustainable regeneration, we're redefining the way we preserve and replenish our planet's most precious resource.",
+  },
+  {
+    imgUrl: '/water-treatment-02.png',
+    title: 'Sustainable Solutions for Tomorrow',
+    subtitle:
+       "Our eco-friendly approach to water treatment ensures a sustainable future for generations to come. By harnessing renewable energy and cutting-edge technology, we're paving the way for a world where clean water is abundant and accessible to all.",
+  },
+  {
+    imgUrl: '/water-treatment-03.png',
+    title: 'Leading the Charge Towards Water Resilience',
+    subtitle:
+        "Ecogenesis is leading the charge in building resilience against water scarcity and pollution. With a commitment to innovation and environmental stewardship, we're shaping a future where water is not just a resource, but a source of life and vitality for all.",
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About EcoGenesis" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        Welcome to <span className="font-extrabold text-white">EcoGenesis</span>, 
+        where innovation meets sustainability! <br></br>At EcoGenesis, 
+        we are dedicated to revolutionizing the way we approach water treatment and conservation.we strive to address the pressing challenges of water scarcity and pollution plaguing our planet.<br></br>
+        <span className="font-extrabold text-white">
+        Our mission
+        </span>{' '} 
+        is to create a world where water is not just a resource but a source of life, vitality, and harmony with nature. 
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
